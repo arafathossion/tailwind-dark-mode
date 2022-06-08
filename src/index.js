a86b7a57146bd38cc8a3bfc679ce9a56cb26e1f1
arafathossion <arafathossionrahman@gmail.com>
2022-06-08 20:35:35 +0600

    readme change

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from './Components/ThemeContext';
-import Navbar from './Components/Navbar';
+
 
 ReactDOM.render(
   <React.StrictMode>
